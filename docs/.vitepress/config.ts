b36a419bbb0301c644ae30d201b3920acb4f7c2c
wurongjie <wurongjie@deepin.org>
2023-03-31 14:03:35 +0800

    fix: doc footer type config error
    docFooter应该传递对象而不是对象数组
    
    Log:

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -83,12 +83,10 @@ export default defineConfig({
         ],
       },
     ],
-    docFooter: [
-      {
-        prev: '上一页',
-        next: '下一页'
-      }
-    ],
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
     darkModeSwitchLabel: '浅色/深色模式',
     outlineTitle: '此页的大纲',
     sidebarMenuLabel: '菜单',
